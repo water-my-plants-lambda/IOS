d1ac21449ed924a4a90f97fecd909168f05962dc
Mitchell Budge <mitchellbudge@gmail.com>
2019-05-30 09:18:42 -0600

    Created ThemeHelper and added base UIColors to match theme

---
+++ Water My Plants/Water My Plants/Model/ThemeHelper.swift
@@ -0,0 +1,21 @@
+//
+//  ThemeHelper.swift
+//  Water My Plants
+//
+//  Created by Mitchell Budge on 5/30/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+import UIKit
+
+enum ThemeHelper {
+    static var primaryGreen = UIColor(red: 51/255, green: 105/255, blue: 30/255, alpha: 1.0)
+    static var lightGreen = UIColor(red: 98/255, green: 151/255, blue: 73/255, alpha: 1.0)
+    static var darkGreen = UIColor(red: 0/255, green: 61/255, blue: 0/255, alpha: 1.0)
+    static var secondaryBeige = UIColor(red: 255/255, green: 229/255, blue: 127/255, alpha: 1.0)
+    static var lightBeige = UIColor(red: 255/255, green: 255/255, blue: 176/255, alpha: 1.0)
+    static var darkBeige = UIColor(red: 202/255, green: 179/255, blue: 80/255, alpha: 1.0)
+    
+}
+
+
